7f4a5ef3654fa7bd7689ced88a9eae327f879068
github username <github email address>
2019-03-02 20:15:09 -0500

    Done.

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ You should NOT try to redefine any of the above variables.
 */
 
 // Write your code below this line!
+newID = oldID + 1000000000;
+
+ageIsValid = Number.isInteger(currentAge);
+
+randomNumber = Math.random(0) * 20 + 1;
+
+randomInteger = Math.floor(randomNumber);
+
+randomUserID = Math.floor(randomNumber + 1000000000)
